1807620047be5f497ecf2b8f5201f0347e4caaf3
Stefan Dzhestanov <stefan.dzhestanov@dojo.tech>
2024-07-16 21:02:49 +0100

    Adds a deploy script

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -5,14 +5,13 @@ pragma solidity ^0.8.26;
 import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagelToken} from "../src/BagelToken.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
 contract MerkleAirdropTest is Test {
     MerkleAirdrop public airdrop;
     BagelToken public token;
 
-    bytes32 public MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 AMOUNT_TO_CLAIM = 25 * 1e18;
-    uint256 AMOUNT_TO_MINT = AMOUNT_TO_CLAIM * 4;
     bytes32 firstProof = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 secondProof = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [firstProof, secondProof];
@@ -21,10 +20,8 @@ contract MerkleAirdropTest is Test {
     uint256 userPrivateKey;
 
     function setUp() public {
-        token = new BagelToken();
-        airdrop = new MerkleAirdrop(MERKLE_ROOT, token);
-        token.mint(token.owner(), AMOUNT_TO_MINT);
-        token.transfer(address(airdrop), AMOUNT_TO_MINT);
+        DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+        (airdrop, token) = deployer.run();
         (user, userPrivateKey) = makeAddrAndKey("user");
     }
 
