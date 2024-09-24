de27f36776f3b5830d1fbc7fe2c88b6712de1051
Stefan Dzhestanov <stefan.dzhestanov@dojo.tech>
2024-09-24 19:06:21 +0100

    Implement signatures

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -6,29 +6,45 @@ import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {BagelToken} from "../src/BagelToken.sol";
 import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
 
-contract MerkleAirdropTest is Test {
+contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     MerkleAirdrop public airdrop;
     BagelToken public token;
 
+    bytes32 private MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 private AMOUNT_TO_TRANSFER = 4 * AMOUNT_TO_CLAIM;
     bytes32 firstProof = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 secondProof = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [firstProof, secondProof];
 
     address user;
+    address public gasPayer;
     uint256 userPrivateKey;
 
     function setUp() public {
-        DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
-        (airdrop, token) = deployer.run();
+        if  (!isZkSyncChain()) {
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.run();
+        } else {
+            token = new BagelToken();
+            airdrop = new MerkleAirdrop(MERKLE_ROOT, token);
+            token.mint(token.owner(), AMOUNT_TO_TRANSFER);
+            token.transfer(address(airdrop), AMOUNT_TO_TRANSFER);
+        }
         (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        // sign the message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("Ending balance", endingBalance);
